20a19c5c4c1e1d7cd13c10dbee145f687c07f8d5
aguallarcdam1 <aguallarcdam1@gmail.com>
2021-03-28 12:07:50 +0200

    Primer commit saludo a Juan

---
+++ src/prueba/modelo/Saludo.java
@@ -0,0 +1,19 @@
+package prueba.modelo;
+
+/**
+ * Modela un saludo personalizado
+ * 
+ * 
+ */
+
+public class Saludo {
+
+	 
+	public Saludo() {
+
+	}
+ 
+	public String  saludar(String nombre) {
+		return "Hola " + nombre;
+	}
+}
---
+++ src/prueba/test/PruebaGitEclipse.java
@@ -0,0 +1,14 @@
+package prueba.test;
+
+import prueba.modelo.Saludo;
+
+public class PruebaGitEclipse {
+
+	public static void main(String[] args) {
+		Saludo saludo = new Saludo();
+		System.out.println(saludo.saludar("Juan"));
+		 
+
+	}
+
+}
